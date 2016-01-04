09125f7d8d0f9b5852d2835d86ea9d9b4ca0b895
san02 <shangeo93@gmail.com>
2016-01-04 16:33:42 +0100

    third commit

---
+++ Script/PointPat_Densities.R
@@ -0,0 +1,42 @@
+#Point pattern densities - estimation of densities using spatstat
+
+tool_exec <- function(in_params, out_params)
+{
+  if (!requireNamespace("sp", quietly = T))
+    install.packages("sp")
+  if (!requireNamespace("spatstat",quietly = T))
+    install.packages("spatstat")
+  if (!requireNamespace("maptools",quietly = T))
+    install.packages("maptools")
+  if (!requireNamespace("raster",quietly = T))
+    install.packages("raster")
+  
+  require(sp)
+  require(gstat)
+  require(spatstat)
+  require(maptools)
+  require(raster)
+  
+  input_feature = in_params[[1]]
+  dep_variable = in_params[[2]]
+  output_feature = out_params[[1]]
+  d = arc.open(input_feature)
+  dat = arc.select(d, dep_variable)
+  dat.xy = data.frame(x=arc.shape(dat)$x,y=arc.shape(dat)$y,input_feature)
+  dat.1 = arc.select(d,names(d@fields[d@fields == "OID"]))
+  dat.2 = cbind(dat.1,dat.xy,dat)
+  coordinates(dat.2)=~x+y
+  
+  message("loading...",class(dat.xy))
+  
+   try1 = as.ppp(dat.2)
+  d= density.ppp(try1, sigma = 70)
+  r = raster(d)
+  patternDensity = as(r, "SpatialPolygonsDataFrame")
+  
+  #### Write Output ####
+  message("...write output...")
+  arc.write(output_feature,patternDensity)
+  message("...done...almost...")
+  return(out_params)
+}
