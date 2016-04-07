8dfd1ddeeacb09353e97069787e8e811d3eb87b3
san02 <shangeo93@gmail.com>
2016-04-07 17:37:46 +0200

    added window

--- Script/PointPattern.R
+++ Script/PointPattern.R
@@ -10,42 +10,63 @@ tool_exec <- function(in_params, out_params)
     install.packages("maptools")
   if (!requireNamespace("raster",quietly = T))
     install.packages("raster")
-  
+
   require(sp)
   require(spatstat)
   require(maptools)
   require(raster)
-  
+
   ##Declaring the inputs and Output
   input_feature = in_params[[1]]
+  input_boundary = in_params[[2]]
+  BWidth = in_params[[3]]
   output_feature1 = out_params[[1]]
   output_feature2 = out_params[[2]]
-  
+
   ## Reading the input in Arcgis
   d = arc.open(input_feature)
   dat = arc.select(d, names(d@fields))
   dat.2 = arc.data2sp(dat)
-  
-  ### Converting the class   
-  dat.ppp = as.ppp(dat.2)
+
+  #reading boudary input
+  b = arc.open(input_boundary)
+  b1 = arc.select(b,names(b@fields))
+  b2 = arc.data2sp(b1)
+
+  ##converting boundary into owin object
+  b.win = as.owin(b2)
+
+  ### Converting the class
+  if(!is.null(input_boundary))
+  {
+    pts = coordinates(dat.2)
+    dat.ppp = ppp(pts[,1],pts[,2],window = b.win)
+   }
+  else
+  {
+    dat.ppp = as.ppp(dat.2)
+  }
+
   print(dat.ppp)
-  d1= density.ppp(dat.ppp, sigma = 70)
-  print(d1) 
+
+  # Estimating Density
+  d1= density.ppp(dat.ppp, sigma = BWidth)
+  print(d1)
   r = raster(d1)
-  
+
   ## Converting to SP class
   patternDensity = as(r, "SpatialPolygonsDataFrame")
-  
+
   ##Simulating point pattern with the density as reference
   fitDensity = ppm(unmark(dat.ppp),~d1)
   simDensity = simulate(fitDensity)
   print(simDensity)
-  
+
   ##converting to dataframe
   simDensDataF = as.data.frame(simDensity)
-  spdfSim = SpatialPointsDataFrame(simDensDataF,simDensDataF) 
+  spdfSim = SpatialPointsDataFrame(simDensDataF,simDensDataF)
   #### Write Output ####
-  
+
   message("...write output...")
   arc.write(output_feature1,patternDensity)
   arc.write(output_feature2,spdfSim, shape_info = d@shapeinfo)
